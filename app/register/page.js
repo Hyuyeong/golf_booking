71b142892b22f48a067a5a9363d84d0f1b47a415
Hyuyeong <choibeny@gmail.com>
2025-04-28 00:36:53 +1200

    fix resister-r

--- app/register/page.js
+++ app/register/page.js
@@ -12,7 +12,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/register`, {
+    const res = await fetch("/api/auth/register", {
       method: "POST",
       body: JSON.stringify({ email, username, password }),
       headers: {
